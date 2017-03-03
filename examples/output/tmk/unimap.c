232dc2a92d262f1c7293a6b2aba9262560b9ce74
Aldo Gunsing <dev@aldogunsing.nl>
2017-03-03 22:14:15 +0100

    Updated example output

--- examples/output/tmk/unimap.c
+++ examples/output/tmk/unimap.c
@@ -34,7 +34,7 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
      GRV,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,JYEN,BSPC,      INS,HOME,PGUP,    NLCK,PSLS,PAST,PMNS,
      TAB,   Q,   W,   F,   P,   G,   J,   L,   U,   Y,SCLN,LBRC,RBRC,     BSLS,      DEL, END,PGDN,      P7,  P8,  P9,PPLS,
      FN0,   A,   R,   S,   T,   D,   H,   N,   E,   I,   O,QUOT,     NUHS, ENT,                          P4,  P5,  P6,PCMM,
-     FN1,MINS,   Z,   X,   C,   V,   B,   K,   M,COMM, DOT,SLSH,       RO, FN2,            UP,           P1,  P2,  P3,PENT,
+     FN1,NUBS,   Z,   X,   C,   V,   B,   K,   M,COMM, DOT,SLSH,       RO, FN2,            UP,           P1,  P2,  P3,PENT,
     LCTL,LGUI,LALT,MHEN,           SPC,          HENK,KANA,RALT,RGUI, APP,RCTL,     LEFT,DOWN,RGHT,      P0,     PDOT,PEQL
     ),
     // Extend
@@ -44,7 +44,7 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
       NO,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12,TRNS,TRNS,     TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS,
     TRNS, ESC,WH_U,WBAK,WFWD,MS_U,PGUP,HOME,  UP, END, DEL, ESC, INS,     WFAV,     TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS,
     TRNS,LALT,WH_D, FN3,LCTL,MS_D,PGDN,LEFT,DOWN,RGHT,BSPC, APP,     NUHS,TRNS,                        TRNS,TRNS,TRNS,TRNS,
-    TRNS,  NO, FN4, FN5, FN6, FN7,BTN1,BTN2,BTN3,MS_L,MS_R,  NO,     TRNS,TRNS,          TRNS,         TRNS,TRNS,TRNS,TRNS,
+    TRNS,NUBS, FN4, FN5, FN6, FN7,BTN1,BTN2,BTN3,MS_L,MS_R,  NO,     TRNS,TRNS,          TRNS,         TRNS,TRNS,TRNS,TRNS,
     TRNS,TRNS,TRNS,TRNS,           ENT,          TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,TRNS,TRNS,    TRNS,     TRNS,TRNS
     ),
     // Shift+Extend
@@ -54,7 +54,7 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS,
     TRNS,TRNS,WH_R,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     MYCM,     TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS,
     TRNS,TRNS,WH_L,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     NUHS,TRNS,                        TRNS,TRNS,TRNS,TRNS,
-    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,TRNS,          TRNS,         TRNS,TRNS,TRNS,TRNS,
+    TRNS,NUBS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,TRNS,          TRNS,         TRNS,TRNS,TRNS,TRNS,
     TRNS,TRNS,TRNS,TRNS,          TRNS,          TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,TRNS,TRNS,    TRNS,     TRNS,TRNS
     ),
 };
