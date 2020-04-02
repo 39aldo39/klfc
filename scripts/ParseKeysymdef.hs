690aba3b185059a620d0d338345fff73bb10dd00
Aldo Gunsing <dev@aldogunsing.nl>
2020-04-02 23:06:21 +0200

    Update keysymdef parsing (#28)

--- scripts/ParseKeysymdef.hs
+++ scripts/ParseKeysymdef.hs
@@ -5,28 +5,30 @@ module ParseKeysymdef where
 import Prelude.Unicode
 
 import Data.Char (chr)
+import Data.Void (Void)
 import Text.Read (readMaybe)
-import qualified Data.Text.Lazy.IO as L (readFile)
 import Control.Applicative (liftA2)
 import Text.Megaparsec
-import Text.Megaparsec.Text.Lazy (Parser)
+import Text.Megaparsec.Char
+
+type Parser = Parsec Void String
 
 main ∷ IO ()
 main =
     parseFromFile symLines "/usr/include/X11/keysymdef.h" >>=
-    either (print ∘ parseErrorPretty) (mapM_ (\(s,c) → putStrLn (replicate 4 ' ' ⧺ ", ('" ⧺ [c] ⧺ "', " ⧺ show s ⧺ ")")))
+    either (print ∘ errorBundlePretty) (mapM_ (\(s,c) → putStrLn (replicate 4 ' ' ⧺ ", ('" ⧺ [c] ⧺ "', " ⧺ show s ⧺ ")")))
 
 symLine ∷ Parser [(String, Char)]
 symLine = (:[]) <$> liftA2 (,)
           (string "#define XK_" *> many (alphaNumChar <|> char '_') <* space <* many alphaNumChar <* space)
-          (fmap f (string "/*" *> space *> optional (char '(') *> space *> string "U+" *> many alphaNumChar <* manyTill anyChar eol))
+          (fmap f (string "/*" *> space *> string "U+" *> many alphaNumChar <* manyTill anySingle eol))
     where f xs = maybe '\0' chr (readMaybe ('0':'x':xs))
 
 symLines ∷ Parser [(String, Char)]
-symLines = concat <$> many (try symLine <|> [] <$ manyTill anyChar eol)
+symLines = concat <$> many (try symLine <|> [] <$ manyTill anySingle eol)
 
 printLines ∷ [(String, Char)] → String
 printLines = unlines ∘ map (\(s, c) → "    , ('" ⧺ [c] ⧺ "', " ⧺ show s ⧺ ")")
 
-parseFromFile ∷ Parser α → String → IO (Either (ParseError Char Dec) α)
-parseFromFile p fname = parse p "" <$> L.readFile fname
+parseFromFile ∷ Parser α → String → IO (Either (ParseErrorBundle String Void) α)
+parseFromFile p fname = parse p "" <$> readFile fname
